e0c6e5cdb427a3c36cf9282ac521794dfa3feeec
sherrychin <kmg31815@gmail.com>
2022-11-25 15:20:35 +0800

    讓api模組能夠用到domain模組的內容

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# com.springboot.ddd.demo2
+# Gradle 多模組專案
 
-Gradle 多模組專案
+#### 測試 api 專案
+```bash
+.\gradlew api:build
+.\gradlew api:bootRun
+```
--- api/src/main/java/org/example/api/ApiApplication.java
+++ api/src/main/java/org/example/api/ApiApplication.java
@@ -2,8 +2,10 @@ package org.example.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "org.example") // 讓 api 模組可以 scan 到 domain 那層
 public class ApiApplication {
 
 	public static void main(String[] args) {
